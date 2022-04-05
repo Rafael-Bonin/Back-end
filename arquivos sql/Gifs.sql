dcd849cf927ea48580d304305ecff2d360cf406b
Rafael <rafalyf11@gmail.com>
2022-04-05 11:56:05 -0300

    exercicios genericcos

---
+++ arquivos sql/Gifs.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(address, ', ', district) AS Endereco FROM address;
+SELECT CONCAT(title, ', ', rating) AS Classificacao FROM film;
+SELECT CONCAT(title, ', ', release_year) AS 'Lancamento do Filme' FROM film;
+SELECT CONCAT(first_name, ' ', last_name) AS 'Nome Completo' FROM sakila.actor;
+SELECT * FROM film;
+SELECT title, description, release_year FROM film;
+SELECT * FROM rental;
+SELECT first_name, last_name FROM customer
