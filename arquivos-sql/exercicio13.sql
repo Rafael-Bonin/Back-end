83be14cae8c83220137c4b46feaa89bdee77e0a1
Rafael <rafalyf11@gmail.com>
2022-04-05 12:01:35 -0300

    ex 13

---
+++ arquivos-sql/exercicio13.sql
@@ -0,0 +1,2 @@
+SELECT * FROM payment WHERE customer_id IN (269, 239, 126, 399, 142);
+SELECT * FROM address WHERE district IN ('QLD', 'Nagasaki', 'California', 'Attika', 'Mandalay', 'Nantou', 'Texas');B
