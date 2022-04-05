4cc0d11b113c5cc03fdaf20ebf6a969b8ec27d51
Rafael <rafalyf11@gmail.com>
2022-04-05 12:28:25 -0300

    exercicio insert

---
+++ arquivos-sql/exercicio1INSERT.sql
@@ -0,0 +1,3 @@
+INSERT INTO staff (first_name, last_name, address_id, email, store_id, active, username, password)
+VALUES ('Rafael', 'Bonin', 5, 'rafalyf11@gmail.com', 1, 1, 'Esuran', 7445062);
+SELECT * from staff;
