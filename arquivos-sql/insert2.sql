37dbaa1568fb7ac7fd3533dc09d248e092bf109c
Rafael <rafalyf11@gmail.com>
2022-04-05 12:31:54 -0300

    ex 2 insert

---
+++ arquivos-sql/insert2.sql
@@ -0,0 +1,4 @@
+INSERT INTO staff (first_name, last_name, address_id, email, store_id, active, username, password)
+VALUES ('antonio', 'feio', 5, 'antoniofeio@gmail.com', 2, 1, 'tonhaofeiaio', 'abc5178'),
+('magenta', 'lazarenta', 6, 'gentarenta@gmai.com', 2, 0, 'marenta', 'soumarrenta123');
+SELECT * FROM staff;
