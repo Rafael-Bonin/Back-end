21f93a6e69bb04cd70f79ef3d830854870d979bc
Rafael <rafalyf11@gmail.com>
2022-04-06 17:59:44 -0300

    funcoes de agregacao

---
+++ 21.1/agregacaoFIxar.sql
@@ -0,0 +1,5 @@
+SELECT AVG(length) AS 'media de duracao' FROM film;
+SELECT MIN(length) AS 'minima duracao' FROM film;
+SELECT MAX(length) AS 'maxima duracao' FROM film;
+SELECT SUM(length) AS 'tempo de exibicao total' FROM film;
+SELECT COUNT(length) AS 'filmes registrados' FROM film;
