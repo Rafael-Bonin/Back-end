324a53e7d1ed7af5afa34651c85b902ca3441506
Rafael <rafalyf11@gmail.com>
2022-04-19 20:15:04 -0300

    exercicio 3 22.4

--- 22.4/exercicios/index.js
+++ 22.4/exercicios/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { response } = require('express');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,6 +10,16 @@ app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   return res.status(200).json({ message: `hello ${name}!` })
+})
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (parseInt(age, 10) <= 17) {
+    return res.status(401).json({ message: `unauthorized` });
+  }
+  if (parseInt(age, 10) >= 18) {
+  return res.status(200).json({ message: `hello, ${name}` });
+  }
 })
 
 app.listen(3001, () => {
